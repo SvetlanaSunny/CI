5cc18a0ec1181179122ad576a058dd07e880cd67
Светлана Чернова <lana_19.87@mail.ru>
2022-04-07 15:12:14 +0600

    second commit

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,5 +3,7 @@ package com;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hellow world");
+        System.out.println("Мы подключили cit - hub actions");
+        var:=1;
     }
 }
