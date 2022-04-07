29d1e81f393fe71911176c3080140669421e5030
Светлана Чернова <lana_19.87@mail.ru>
2022-04-07 15:20:14 +0600

    fix error

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hellow world");
         System.out.println("Мы подключили cit - hub actions");
-        var:=1;
-    }
+       }
 }
